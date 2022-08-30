4163ca46ad12a08c6db4c2c9d34bbc0eb720bbdf
Mountain Star <github@mtnstar.net>
2022-08-30 22:37:02 +0200

    Seed test fixtures in development env

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+def load_fixtures
+  puts 'Seeding all fixtures...'
+
+  require 'active_record/fixtures'
+
+  fixtures_dir = Rails.root.join('spec/fixtures').to_s
+
+  # The use of String#[] here is to support namespaced fixtures.
+  fixture_files = Dir["#{fixtures_dir}/**/*.yml"].map { |f| f[(fixtures_dir.size + 1)..-5] }
+
+  ActiveRecord::FixtureSet.create_fixtures(fixtures_dir, fixture_files)
+end
+
+load_fixtures
