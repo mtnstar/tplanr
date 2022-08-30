b63d66771bcb6696d13e99a16108c72f5342cab6
Mountain Star <github@mtnstar.net>
2022-08-30 22:36:34 +0200

    Add admin boolean to user

--- backend/db/migrate/20220830195811_devise_create_users.rb
+++ backend/db/migrate/20220830195811_devise_create_users.rb
@@ -5,6 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name, null: false
       t.string :last_name, null: false
+      t.boolean :admin, default: false, null: false
 
       ## Database authenticatable
       t.string :email,              null: false
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_195811) do
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.boolean "admin", default: false, null: false
     t.string "email", null: false
     t.string "encrypted_password", null: false
     t.string "reset_password_token"
