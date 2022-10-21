214ca267492775adb519a17ec161b1efedfd9212
Mountain Star <github@mtnstar.net>
2022-10-21 10:28:01 +0200

    Fix backend specs

--- backend/spec/routing/item_categories_routing_spec.rb
+++ backend/spec/routing/item_categories_routing_spec.rb
@@ -3,28 +3,28 @@ require "rails_helper"
 RSpec.describe ItemCategoriesController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/item_categories").to route_to("item_categories#index")
+      expect(get: "/api/item_categories").to route_to("item_categories#index")
     end
 
     it "routes to #show" do
-      expect(get: "/item_categories/1").to route_to("item_categories#show", id: "1")
+      expect(get: "/api/item_categories/1").to route_to("item_categories#show", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/item_categories").to route_to("item_categories#create")
+      expect(post: "/api/item_categories").to route_to("item_categories#create")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/item_categories/1").to route_to("item_categories#update", id: "1")
+      expect(put: "/api/item_categories/1").to route_to("item_categories#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/item_categories/1").to route_to("item_categories#update", id: "1")
+      expect(patch: "/api/item_categories/1").to route_to("item_categories#update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/item_categories/1").to route_to("item_categories#destroy", id: "1")
+      expect(delete: "/api/item_categories/1").to route_to("item_categories#destroy", id: "1")
     end
   end
 end
--- backend/spec/routing/items_routing_spec.rb
+++ backend/spec/routing/items_routing_spec.rb
@@ -3,28 +3,28 @@ require "rails_helper"
 RSpec.describe ItemsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/items").to route_to("items#index")
+      expect(get: "/api/items").to route_to("items#index")
     end
 
     it "routes to #show" do
-      expect(get: "/items/1").to route_to("items#show", id: "1")
+      expect(get: "/api/items/1").to route_to("items#show", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/items").to route_to("items#create")
+      expect(post: "/api/items").to route_to("items#create")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/items/1").to route_to("items#update", id: "1")
+      expect(put: "/api/items/1").to route_to("items#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/items/1").to route_to("items#update", id: "1")
+      expect(patch: "/api/items/1").to route_to("items#update", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/items/1").to route_to("items#destroy", id: "1")
+      expect(delete: "/api/items/1").to route_to("items#destroy", id: "1")
     end
   end
 end
