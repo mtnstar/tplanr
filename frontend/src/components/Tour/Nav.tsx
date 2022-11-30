126e15a4c5cf64705244995c22a7e67106456563
Mountain Star <github@mtnstar.net>
2022-12-01 11:33:55 +1300

    Add tour breadcrumb, update tour nav

--- frontend/src/components/Tour/Nav.tsx
+++ frontend/src/components/Tour/Nav.tsx
@@ -1,40 +1,75 @@
 import Nav from 'react-bootstrap/Nav';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import { useTranslation } from 'react-i18next';
-import { Link, useParams } from 'react-router-dom';
-import Tour from '../../model/Tour';
+import { NavLink, Link, useParams } from 'react-router-dom';
 import { useTourQuery } from '../../utils/queries/useTourQuery';
+import Tour from '../../model/Tour';
+
+interface BreadcrumbsParams {
+  tour: Tour | undefined;
+}
+
+function Breadcrumbs(props: BreadcrumbsParams) {
+  const { t } = useTranslation();
+  const { tour } = props;
+  return (
+    <Breadcrumb>
+      <Breadcrumb.Item>
+        <Link className='nav-link' to={'/tours'}>
+          {t('many', { keyPrefix: 'tour' })}
+        </Link>
+      </Breadcrumb.Item>
+      <li className='breadcrumb-item'>
+        {tour ? tour.label : t('new', { keyPrefix: 'tour' })}
+      </li>
+    </Breadcrumb>
+  );
+}
 
 function TourNav() {
-  const { id } = useParams();
+  const linkClassName = (active: boolean) => {
+    let classes = ['nav-link'];
+    if (active) classes.push('active');
+    return classes.join(' ');
+  };
 
-  const { data } = useTourQuery(+id!);
+  const { id } = useParams();
+  const { data: tour } = useTourQuery(+id!);
   const { t } = useTranslation();
 
-  const tour = data as Tour;
-
-  if (tour) {
-    return (
+  return (
+    <>
+      <Breadcrumbs tour={tour} />
       <Nav variant='tabs'>
         <Nav.Item>
-          <Link className='nav-link active' to={`/tours/${tour.id}`}>
-            {tour.label}
-          </Link>
+          <NavLink
+            className={({ isActive }) => linkClassName(isActive)}
+            to={tour ? `/tours/${tour.id}` : ''}
+            end
+          >
+            {t('details', { keyPrefix: 'tour' })}
+          </NavLink>
         </Nav.Item>
         <Nav.Item>
-          <Link className='nav-link' to={`/tours/${tour.id}/sections`}>
-            {t('many', { keyPrefix: 'section' })}
-          </Link>
+          {tour && (
+            <Link className='nav-link' to={`/tours/${tour.id}/sections`}>
+              {t('many', { keyPrefix: 'section' })}
+            </Link>
+          )}
         </Nav.Item>
         <Nav.Item>
-          <Link className='nav-link' to={`/tours/${tour.id}/items`}>
-            {t('many', { keyPrefix: 'item' })}
-          </Link>
+          {tour && (
+            <NavLink
+              className={({ isActive }) => linkClassName(isActive)}
+              to={`/tours/${tour.id}/items`}
+            >
+              {t('many', { keyPrefix: 'item' })}
+            </NavLink>
+          )}
         </Nav.Item>
       </Nav>
-    );
-  }
-
-  return <></>;
+    </>
+  );
 }
 
 export default TourNav;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import TourShow from './components/Tour/Show';
 import TourContainer from './components/Tour/Container';
 import TourEdit from './components/Tour/Edit';
+import ItemList from './components/Item/List';
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,7 @@ const router = createBrowserRouter([
         children: [
           { path: '/tours/:id', element: <TourShow /> },
           { path: '/tours/:id/edit', element: <TourEdit /> },
+          { path: '/tours/:id/items', element: <ItemList /> },
         ],
       },
     ],
