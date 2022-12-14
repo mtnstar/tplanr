e36b813f2feb057c9222c26de48d59614cafd673
Mountain Star <github@mtnstar.net>
2022-12-15 06:34:24 +1300

    Fix date validation for section

--- backend/app/models/section.rb
+++ backend/app/models/section.rb
@@ -4,4 +4,17 @@ class Section < ApplicationRecord
 
   attr_readonly :type
 
+  validates_comparison_of :start_at, greater_than: :tour_start_at, smaller_than: :tour_end_at
+  validates_comparison_of :end_at, greater_than: :start_at, smaller_than: :tour_end_at
+
+  private
+
+  def tour_start_at
+    tour.start_at
+  end
+
+  def tour_end_at
+    tour.end_at
+  end
+
 end
