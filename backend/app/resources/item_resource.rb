7112ff11c0090b713cc58c4fd2f2ad84a52699c6
Mountain Star <github@mtnstar.net>
2023-01-08 08:50:23 +1300

    Only return user and global items

--- backend/app/resources/item_resource.rb
+++ backend/app/resources/item_resource.rb
@@ -1,5 +1,4 @@
 class ItemResource < JSONAPI::Resource
-  # TODO: only return global and user's personal item records
   attributes :label_de, :description_de, :item_category
 
   filters :label_de, :item_category
@@ -7,6 +6,11 @@ class ItemResource < JSONAPI::Resource
     value = value.first.downcase + '%%'
     records.where('LOWER(label_de) LIKE :value', value: value)
   }
+
+  def self.records(options = {})
+    user_id = options[:context][:current_user].id
+    Item.where(user_id: [nil, user_id])
+  end
 
   def self.updatable_fields(context)
     super - [:created_at, :updated_at, :item_category]
