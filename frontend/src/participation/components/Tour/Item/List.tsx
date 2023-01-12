dad438a79cc09b9fb21d456aa29182de7ff81da9
Mountain Star <github@mtnstar.net>
2023-01-13 09:33:15 +1300

    Persist items packed in local storage

--- frontend/src/participation/components/Tour/Item/List.tsx
+++ frontend/src/participation/components/Tour/Item/List.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Card } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
@@ -7,7 +8,6 @@ import { useParticipationTourItemsQuery } from '../../../utils/queries/usePartic
 
 export default function ParticipationTourItemList() {
   const { uid: tourParticipationUid } = useParams();
-  console.log(tourParticipationUid);
   const { data: items } = useParticipationTourItemsQuery(tourParticipationUid!);
   function itemsByCategory(category: ItemCategory) {
     if (!items) return [];
@@ -64,13 +64,66 @@ interface ItemCardParams {
   item: ItemListItem;
 }
 
+function getItemIsPacked(
+  participationUid: string | undefined,
+  item: ItemListItem,
+) {
+  if (!participationUid || !item.id) return false;
+
+  return getItemsPackedEntries(participationUid)[item.id];
+}
+
+function getItemsPackedEntries(participationUid: string): ItemPackedEntries {
+  const storeKey = itemsPackedStoreKey(participationUid);
+  return (
+    JSON.parse(`${localStorage.getItem(storeKey)}`) || ({} as ItemPackedEntries)
+  );
+}
+
+function setItemIsPacked(
+  participationUid: string | undefined,
+  item: ItemListItem,
+  isPacked: boolean,
+) {
+  if (!participationUid || !item.id) return;
+
+  const itemsPackedEntries = getItemsPackedEntries(participationUid);
+  itemsPackedEntries[`${item.id}`] = isPacked;
+
+  const storeKey = itemsPackedStoreKey(participationUid);
+  localStorage.setItem(storeKey, JSON.stringify(itemsPackedEntries));
+}
+
+interface ItemPackedEntries {
+  [key: string]: boolean;
+}
+
+function itemsPackedStoreKey(participationUid: string) {
+  return `itemsPacked-${participationUid}`;
+}
+
 function ItemCard(props: ItemCardParams) {
   const { item } = props;
+  const { uid: participationUid } = useParams();
+  const [isPacked, setIsPacked] = useState(
+    getItemIsPacked(participationUid, item),
+  );
+
+  function itemPacked(isPacked: boolean) {
+    setItemIsPacked(participationUid, item, isPacked);
+    setIsPacked(isPacked);
+  }
 
   return (
-    <Card className='mb-2 col'>
+    <Card className='mb-2 col-12'>
       <Card.Body className='d-flex justify-content-between'>
         <div>
+          <input
+            type='checkbox'
+            className='me-2'
+            onChange={(e) => itemPacked(e.target.checked)}
+            checked={isPacked}
+          />
           <span className='item-label'>
             {item.count}x {item.labelDe}
           </span>
